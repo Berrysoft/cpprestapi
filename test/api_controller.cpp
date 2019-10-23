e0c3ff9824e801055bcb5b9012f619f4cc0d7bba
Berrysoft <Strawberry_Str@hotmail.com>
2019-10-23 12:14:40 +0800

    Confirm type convert.

--- test/api_controller.cpp
+++ test/api_controller.cpp
@@ -6,20 +6,26 @@ using namespace web::http;
 using namespace utility;
 
 template <typename _Tp, typename _Class, typename... _Args>
-constexpr decltype(auto) mem_fn_bind(_Class (_Tp::*__f)(_Args...), _Tp* __t)
+constexpr std::function<_Class(_Args...)> mem_fn_bind(_Class (_Tp::*__f)(_Args...), _Tp* __t)
 {
-    return [=](_Args... args) { return (__t->*__f)(args...); };
+    return [=](_Args... args) { return (__t->*__f)(std::move(args)...); };
 }
 
 api_controller::api_controller()
 {
-    this->support(U("/hello"), function<void(http_request)>(mem_fn_bind(&api_controller::get_hello, this)));
-    this->support(U("/hello/{0}"), function<void(http_request, string_t)>(mem_fn_bind(&api_controller::get_hello_name, this)));
+    this->support(U("/hello"), mem_fn_bind(&api_controller::get_hello, this));
+    this->support(U("/hello/{0}"), mem_fn_bind(&api_controller::get_hello_name, this));
+    this->support(U("/hello/{0}/{1}"), mem_fn_bind(&api_controller::get_hello_name_times, this));
 }
 
-void api_controller::get_hello_name(web::http::http_request message, utility::string_t name)
+void api_controller::get_hello_name_times(http_request message, string_t name, size_t times)
 {
     ostringstream_t stream;
-    stream << U("Hello, ") << name << U('!');
+    stream << U("Hello, ") << name;
+    if (times)
+    {
+        stream << U(", ") << times << U(" times");
+    }
+    stream << U('!');
     message.reply(status_codes::OK, stream.str());
 }
--- test/api_controller.hpp
+++ test/api_controller.hpp
@@ -9,7 +9,8 @@ public:
     api_controller();
 
     void get_hello(web::http::http_request message) { get_hello_name(std::move(message), U("world")); }
-    void get_hello_name(web::http::http_request message, utility::string_t name);
+    void get_hello_name(web::http::http_request message, utility::string_t name) { get_hello_name_times(std::move(message), std::move(name), 0); }
+    void get_hello_name_times(web::http::http_request message, utility::string_t name, std::size_t times);
 };
 
 #endif // !CPPRESTAPI_TEST_API_CONTROLLER
